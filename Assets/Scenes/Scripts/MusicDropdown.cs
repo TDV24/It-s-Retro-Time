39b543fe3082a05d4a108dac635f0a6c60a8d61e
Ilie-Tudor <tudor_alex.ilie@yahoo.com>
2023-01-15 23:59:52 +0200

    Solving Build music bug

--- Assets/Scenes/Scripts/MusicDropdown.cs
+++ Assets/Scenes/Scripts/MusicDropdown.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 
 public class MusicDropdown : MonoBehaviour
 {
-    string[] audioFiles;
+    string[] audioFiles = new string[] { "Ocean_Drive", "Party_Rock", "Retro_Funk", "RocknRoll" };
     [SerializeField] AudioClip[] audioClips;
     MusicManager musicManager;
 
@@ -15,14 +15,11 @@ public class MusicDropdown : MonoBehaviour
     void Awake()
     {
         TMP_Dropdown dropdown = transform.GetComponent<TMP_Dropdown>();
-        audioFiles = getAudioFiles();
-
-        char[] delimiterChars = { '/', '\\' };
         dropdown.options.Clear();
 
         foreach (string song in audioFiles)
         {
-            dropdown.options.Add(new TMP_Dropdown.OptionData() { text = System.IO.Path.GetFileName(song).Replace(".mp3", "") });
+            dropdown.options.Add(new TMP_Dropdown.OptionData() { text = song });
         }
 
         dropdown.onValueChanged.AddListener(delegate { DropdownItemSelect(dropdown); });
@@ -53,14 +50,6 @@ public class MusicDropdown : MonoBehaviour
                 break;
         }
         label.text = newSongName;
-    }
-
-    private string[] getAudioFiles()
-    {
-        string path = Application.dataPath + "/Music/AudioFiles";
-        string[] filePaths = System.IO.Directory.GetFiles(@path, "*.mp3");
-
-        return filePaths;
     }
 }
 
