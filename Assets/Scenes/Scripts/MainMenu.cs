76fb8ae7fd5edb8b1136b6a3e3cf15c9670cc247
AlinaGeo <79804243+AlinaGeo@users.noreply.github.com>
2023-01-15 21:10:54 +0200

    Fixed music bug

--- Assets/Scenes/Scripts/MainMenu.cs
+++ Assets/Scenes/Scripts/MainMenu.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private AudioMixer audioMixer;
+    private void Start()
+    {
+        float startVolume = PlayerPrefs.GetFloat("volume");
+        audioMixer.SetFloat("volume",startVolume);
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
